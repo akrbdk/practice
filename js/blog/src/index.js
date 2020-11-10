439bd868caaa6279106f2e64e66e323fc6f2b19c
alexander.karabudka <akarabudka@mail.ru>
2020-11-10 10:43:32 +0300

    #feature NODE JS - blog

--- js/blog/src/index.js
+++ js/blog/src/index.js
@@ -1,8 +1,19 @@
-console.log('Started!')
-console.log('Started!')
-console.log('Started!')
-console.log('Started!')
+import {HeaderComponent} from './components/header.component'
+import {NavigationComponent} from "./components/navigation.component";
+import {PostsComponent} from "./components/posts.component";
+import {CreateComponent} from "./components/create.component";
+import {FavoriteComponent} from "./components/favorite.component";
 
-async function app() {
-    await fetch('fmk')
-}
+const header = new HeaderComponent('header')
+
+const navigation = new NavigationComponent('navigation')
+
+const posts = new PostsComponent('posts')
+const create = new CreateComponent('create')
+const favorite = new FavoriteComponent('favorite')
+
+navigation.registerTabs([
+    {name: 'create', component: create},
+    {name: 'posts', component: posts},
+    {name: 'favorite', component: favorite}
+])
